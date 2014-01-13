660f22290770e6260c12673f044fca7e253d5dde
jagudits <titusz.jagudits@gmail.com>
2014-01-13 12:31:39 +0100

    Started "Frage stellen/bearbeiten" page

--- QuestionsAndAnswers/QuestionsAndAnswers/Controllers/HomeController.cs
+++ QuestionsAndAnswers/QuestionsAndAnswers/Controllers/HomeController.cs
@@ -19,13 +19,33 @@ namespace QuestionsAndAnswers.Controllers
                          select m;
             ViewBag.users = users;
 
+            // top questions
             var topQuestions = from post
                                in dataContext.user_posts
                                where post.parent_post_id == 0
                                orderby post.ranking_points descending
                                select post;
-            Console.WriteLine(topQuestions.ToString());
             ViewBag.topQuestions = topQuestions;
+
+            // newest questions
+            var newestQuestions = from post
+                                      in dataContext.user_posts
+                                      where post.parent_post_id == 0
+                                      orderby post.created_at descending
+                                      select post;
+            ViewBag.newestQuestions = newestQuestions;
+
+            // newest answers
+            ViewBag.newestAnswers = from post
+                                    in dataContext.user_posts
+                                    where post.parent_post_id != 0
+                                    orderby post.created_at descending
+                                    select post;
+
+            // tags
+            ViewBag.tags = from item
+                           in dataContext.tags
+                           select item;
 
             return View();
         }
